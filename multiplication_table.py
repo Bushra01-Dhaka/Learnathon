63ebf63942a705d54fb4fcdd37a08aec986edb46
Humayra Anjum Bushra <nothingg192@gmail.com>
2024-11-24 14:33:28 +0600

    updated file
    CodeChef array related problem solve

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,33 +1,34 @@
-#
-# def multiplication_table(number):
-#     for i in range(1,11):
-#         print(f"{number} X {i} = {number * i}")
-#
-# number = int(input("Enter the number to get it's multiplication table: "))
-# multiplication_table(number)
+# ******************* Multiplication Table *******************************
+def multiplication_table(number):
+    for i in range(1,11):
+        print(f"{number} X {i} = {number * i}")
+
+number = int(input("Enter the number to get it's multiplication table: "))
+multiplication_table(number)
 
 
 #*********** CODECHEFF problem about Array ************************
 
 # 1 no problem
-# two_value = list(map(int, input().split()))
-# array_len = two_value[0]
-# search_value = two_value[1]
-#
-# print("Array Length: " + str(array_len) + " Search Element: " + str(search_value))
-#
-# array = list(map(int, input("Insert Array Element: ").split()))
-#
-# element_present = False
-#
-# for i in array:
-#     if i == search_value:
-#         element_present = True
-#
-# if element_present:
-#     print("YES")
-# else:
-#     print("NO")
+
+two_value = list(map(int, input().split()))
+array_len = two_value[0]
+search_value = two_value[1]
+
+print("Array Length: " + str(array_len) + " Search Element: " + str(search_value))
+
+array = list(map(int, input("Insert Array Element: ").split()))
+
+element_present = False
+
+for i in array:
+    if i == search_value:
+        element_present = True
+
+if element_present:
+    print("YES")
+else:
+    print("NO")
 
 
 # 2 no: Find maximum in an Array
